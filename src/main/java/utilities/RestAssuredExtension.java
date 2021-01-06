d8a0455d512459d0616696cccf1baac7ef1bcdde
VatsonP <58036174+VatsonP@users.noreply.github.com>
2021-01-06 18:29:34 +0200

    Change method sequence in file

--- src/main/java/utilities/RestAssuredExtension.java
+++ src/main/java/utilities/RestAssuredExtension.java
@@ -52,29 +52,32 @@ public class RestAssuredExtension {
         return null;
     }
 
+    public static ResponseOptions<Response> GetWithQueryParamsWithToken(String url, Map<String, String> pathParams, String token)  {
+        Request.header(new Header("Authorization", "Bearer " + token));
+        Request.queryParams(pathParams);
+        return Request.get(url);
+    }
+
+
+    public static ResponseOptions<Response> PostOpsWithBodyAndPathParams(String url, Map<String, String> pathParams, Map<String, String> body)  {
+        Request.pathParams(pathParams);
+        Request.body(body);
+        return Request.post(url);
+    }
 
     public static ResponseOptions<Response> PUTOpsWithBodyAndPathParams(String url, Map<String,String> body, Map<String,String> pathParams) {
         Request.pathParams(pathParams);
         Request.body(body);
         return Request.put(url);
     }
-    public static ResponseOptions<Response> PostOpsWithBodyAndPathParams(String url, Map<String, String> pathParams, Map<String, String> body)  {
-        Request.pathParams(pathParams);
-        Request.body(body);
-        return Request.post(url);
-    }
+
     public static ResponseOptions<Response> DeleteOpsWithPathParams(String url,Map<String, String> pathParams)  {
         Request.pathParams(pathParams);
         return Request.delete(url);
     }
+
     public static ResponseOptions<Response> GetWithPathParams(String url,Map<String, String> pathParams)  {
         Request.pathParams(pathParams);
-        return Request.get(url);
-    }
-
-    public static ResponseOptions<Response> GetWithQueryParamsWithToken(String url, Map<String, String> pathParams, String token)  {
-        Request.header(new Header("Authorization", "Bearer " + token));
-        Request.queryParams(pathParams);
         return Request.get(url);
     }
 
